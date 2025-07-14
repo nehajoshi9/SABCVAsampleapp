86dc5a4558a79bb03c228e61b4963ed107fad2bf
Neha Joshi <85044713+nehajoshi9@users.noreply.github.com>
2025-07-14 16:39:17 -0400

    Implemented dummy function getEvents in CalendarRepository

--- app/src/main/java/com/example/sabcvasampleapp/presentation/calendar/CalendarRepository.kt
+++ app/src/main/java/com/example/sabcvasampleapp/presentation/calendar/CalendarRepository.kt
@@ -1,5 +1,9 @@
 package com.example.sabcvasampleapp.presentation.calendar
 
-class CalendarRepository {
+import com.example.sabcvasampleapp.presentation.data.EventDetails
 
+class CalendarRepository {
+    fun getEvents () : List<EventDetails> {
+        return listOf()
+    }
 }
